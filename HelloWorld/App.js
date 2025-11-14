66b73b63ff861ab6085d8f81aeff81c55c7aea35
sukant43 <sukant98657@gmail.com>
2025-11-14 12:24:37 +0530

    pressable

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,4 +1,4 @@
-import {View,Text,Image, ImageBackground, ScrollView,Button} from 'react-native';
+import {View,Text,Image, ImageBackground, ScrollView,Button,Pressable} from 'react-native';
 
 export default function App(){
   function click(){
@@ -12,9 +12,10 @@ export default function App(){
   ></Image> */}
   <ImageBackground source={{uri:"https://picsum.photos/id/237/200/300"}} style={{flex:1, height:200,width:200}}/>
 <Text>Hello</Text>
-<Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-</Text>
-<Button title='click' onPress={click} color="midnightblue"/>
+<Button title='click' onPress={click} color="midnightblue" />
+<Pressable onPress={()=>{console.log("hi")} }>
+<Text>Hello</Text>
+</Pressable>
 </ScrollView>
   );
 }
