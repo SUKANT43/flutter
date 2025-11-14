f02b0ced22e2605221146fabba67f381a7a14a85
sukant43 <sukant98657@gmail.com>
2025-11-14 23:34:57 +0530

    box model

--- StylingRN/App.js
+++ StylingRN/App.js
@@ -4,8 +4,13 @@ export default function App(){
     // <View style={styles.containers}>
     //   <Text style={styles.title}>Hello</Text>
     // </View>
-    <View>
-      
+    <View style={styles.containers}>
+      <View style={[styles.lightBlueBox,styles.box]}>
+        <Text>Lightblue box</Text>
+      </View>
+      <View style={[styles.lightGreenBox,styles.box]}>
+        <Text>Lightgreen box</Text>
+      </View>
     </View>
   );
 }
@@ -15,8 +20,22 @@ const styles=StyleSheet.create(
     containers:{
       flex:1,backgroundColor:'plum',padding:60
     },
-    title:{
-
-    }
+    box:{
+      width:100,
+      height:100,
+      padding:10,
+      borderWidth:2,
+      borderColor:"purple",
+      borderStyle:"solid",
+      borderRadius:30
+    },
+    lightBlueBox:{
+      backgroundColor:"lightblue",
+     
+    },
+     lightGreenBox:{
+      backgroundColor:"lightgreen",
+     marginTop:10
+    },
   }
 )
