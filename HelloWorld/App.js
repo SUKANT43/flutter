b1bddb5b2d24a926ee34db73c839b21936b70759
sukant43 <sukant98657@gmail.com>
2025-11-14 12:39:48 +0530

    modal

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,9 +1,12 @@
-import {View,Text,Image, ImageBackground, ScrollView,Button,Pressable} from 'react-native';
-
+import {View,Text,Image, ImageBackground, ScrollView,Button,Pressable,Modal} from 'react-native';
+import { useState } from 'react';
 export default function App(){
   function click(){
     alert('hello')
   }
+
+  const[isVisible,setIsModalVisible]=useState(false)
+
   return(
 <ScrollView style={{flex:1,backgroundColor:"plum",padding:60}}>
   {/* <Text><Text style={{color:"white"}}>Hello</Text> World</Text>
@@ -12,10 +15,15 @@ export default function App(){
   ></Image> */}
   <ImageBackground source={{uri:"https://picsum.photos/id/237/200/300"}} style={{flex:1, height:200,width:200}}/>
 <Text>Hello</Text>
-<Button title='click' onPress={click} color="midnightblue" />
+<Button title='click' onPress={()=>{setIsModalVisible(true)}} color="midnightblue" />
 <Pressable onPress={()=>{console.log("hi")} }>
 <Text>Hello</Text>
 </Pressable>
+<Modal visible={isVisible} animationType='slide' presentationStyle='formSheet'>
+  <View style={{flex:1,backgroundColor:"LightBlue",padding:60}}>
+    <Button title='Close' color="MidnightBlue" onPress={()=>{setIsModalVisible(false)}}></Button>
+  </View>
+</Modal>
 </ScrollView>
   );
 }
