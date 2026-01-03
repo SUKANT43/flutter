c70c077cbf4028726d2e3c54f89d37cce36fe90e
sukant43 <sukant98657@gmail.com>
2026-01-03 18:56:42 +0530

    product card for showing details

--- shop_app_flutter/lib/home_page.dart
+++ shop_app_flutter/lib/home_page.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
-
-class HomePage extends StatelessWidget{
+import './global_variable.dart';
+import './product_card.dart';
+class HomePage extends StatefulWidget{
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  
+  final List<String>list=const[
+    'All',
+    'Addidas',
+    'Nike',
+    'Bata'
+  ];
+
+   
+
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+     String selectedFilter='All';
     final border=const OutlineInputBorder(
                         borderSide: BorderSide(
                           color:Color.fromRGBO(225, 225, 225, 1),
@@ -36,12 +59,53 @@ class HomePage extends StatelessWidget{
                       enabledBorder: border,
                       focusedBorder: border
                     ),
-                    
                   ),
-                  
                 ),
               ],
             ),
+            
+            SizedBox(
+              height: 120,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: list.length,
+                itemBuilder: (context, index) {
+                  final filter=list[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 12),
+                    child: GestureDetector(
+                      onTap:(){
+                        setState(() {
+                            selectedFilter=filter;
+                        });
+                      },
+                      child: Chip(
+                      side: BorderSide(color: Colors.white),
+                      label:Text(filter),
+                      backgroundColor: selectedFilter==filter?Theme.of(context).colorScheme.primary:const Color.fromARGB(66, 228, 223, 223),
+                      padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+                      shape:RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40)
+                      )
+                      ),
+                    ),
+                  
+                  );
+                },
+                ),
+            ),
+          Expanded(
+            child: ListView.builder(
+              itemCount:products.length ,
+              itemBuilder: (context,index){
+                final product=products[index];
+                  return ProductCard(
+                    title:product['title'] as String,
+                    price: product['price'] as double,
+                    image:product['imageUrl'] as String);
+              },
+            ),
+          )
           ],
         ),
       )
--- shop_app_flutter/lib/main.dart
+++ shop_app_flutter/lib/main.dart
@@ -14,7 +14,8 @@ void main(){
         theme: ThemeData(
           fontFamily: 'Lato',
           colorScheme:ColorScheme.fromSeed(
-            seedColor: Color.fromRGBO(254, 206, 1, 1)
+            seedColor: Color.fromRGBO(254, 206, 1, 1),
+            primary: Color.fromRGBO(254, 206, 1, 1)
           ) ,
           inputDecorationTheme:const InputDecorationTheme(
             hintStyle: TextStyle(
@@ -22,6 +23,17 @@ void main(){
               fontSize: 16,
             ),
             prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
+          ),
+          textTheme: TextTheme(
+            titleMedium: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              
+            ),
+            bodySmall: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16
+            )
           )
         ),
         home:HomePage(),
